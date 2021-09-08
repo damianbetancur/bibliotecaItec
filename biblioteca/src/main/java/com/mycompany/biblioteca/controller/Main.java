c1419b5e28218137d7a799d162ca26f4d3695b9d
Damian <dbetancur@digitalhouse.com>
2021-09-08 20:41:12 -0300

    setup inicial

---
+++ biblioteca/src/main/java/com/mycompany/biblioteca/controller/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.biblioteca.controller;
+
+import com.mycompany.biblioteca.view.JFrameLogin;
+
+/**
+ *
+ * @author Belén
+ */
+public class Main {
+    
+    public static void main(String[] args){
+        JFrameLogin app= new JFrameLogin();
+
+    
+}
+    }
---
+++ biblioteca/src/main/java/com/mycompany/biblioteca/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.mycompany.biblioteca.controller;
+
+import com.mycompany.biblioteca.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserController {
+    User userSelected;
+
+    List <User> users;
+
+    public UserController() {
+        this.users = new ArrayList<>();
+        load();
+    }
+
+    private void load(){
+        User u1 = new User("admin", "admin");
+        User u2 = new User("user1", "user1");
+        User u3 = new User("user2", "user2");
+        User u4 = new User("user3", "user3");
+        User u5 = new User("user4", "user4");
+
+        this.users.add(u1);
+        this.users.add(u2);
+        this.users.add(u3);
+        this.users.add(u4);
+        this.users.add(u5);
+
+        for (User u: this.users) {
+            System.out.println(u.getUserName()+" "+ u.getPassword());
+        }
+
+    }
+
+    public User login(User userAux){
+        User userEncontrado = null;
+        for (User ur: this.users) {
+            if (ur.getUserName().equals(userAux.getUserName())){
+                if (ur.getPassword().equals(userAux.getPassword())){
+                    userEncontrado = ur;
+                }else{
+                    System.out.println("la contraseña es invalida");
+                }
+            }else{
+                System.out.println("el usuario no existe");
+            }
+        }
+
+        return userEncontrado;
+    }
+}
