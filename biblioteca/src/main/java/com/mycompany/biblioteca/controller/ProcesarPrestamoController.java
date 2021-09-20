ee92d4aea525819dbed51b4cad5c13ed2dd1259d
Damián Ariel Betancur <da_ariel@hotmail.com>
2021-09-20 20:52:49 -0300

    diagramas 01

--- biblioteca/src/main/java/com/mycompany/biblioteca/controller/ProcesarPrestamoController.java
+++ biblioteca/src/main/java/com/mycompany/biblioteca/controller/ProcesarPrestamoController.java
@@ -5,10 +5,34 @@
  */
 package com.mycompany.biblioteca.controller;
 
+import com.mycompany.biblioteca.model.Biblioteca;
+import com.mycompany.biblioteca.model.Prestamo;
+import com.mycompany.biblioteca.model.Socio;
+
 /**
  *
  * @author Ariel
  */
 public class ProcesarPrestamoController {
+    private Prestamo nuevoPrestamo;
     
+    private Biblioteca biblioteca;
+
+    public ProcesarPrestamoController(Biblioteca biblioteca) {
+        this.biblioteca = biblioteca;
+        this.nuevoPrestamo = new Prestamo();
+    }
+    
+    
+    
+    
+    public Socio buscarSocio(String numeroSocio){
+        Socio socioEncontrado = null;
+        for (Socio socioR : biblioteca.getSocios()) {
+            if (socioR.getNumeroSocio().equals(numeroSocio)) {
+                socioEncontrado=socioR;
+            }
+        }
+        return socioEncontrado;
+    }
 }
